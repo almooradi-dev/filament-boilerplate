e942378a0ef415057f0c0dc36349f996e4c4b13c
aAntarCalo <a.antar@calo.app>
2025-07-01 09:54:58 +0300

    [Page] Some fixes

--- app/Models/Core/Page.php
+++ app/Models/Core/Page.php
@@ -15,34 +15,7 @@ class Page extends Model
 
     public $translatable = ['name', 'content', 'metadata'];
 
-    public $mediaFieldsByBlockType = [ // 'type' => ['fields']
+    public static $mediaFieldsByBlockType = [ // 'type' => ['fields']
         'info_with_images' => ['images'],
     ];
-
-    public function getContentAttribute($value)
-    {
-        $blocks = is_array($value) ? $value : json_decode($value, true);
-
-        // Full storage path
-        $blocksWithMedia = array_keys($this->mediaFieldsByBlockType);
-        foreach ($blocks as $index => $block) {
-            if (!in_array($block['type'], $blocksWithMedia)) {
-                continue;
-            }
-
-            foreach ($this->mediaFieldsByBlockType[$block['type']] as $field) {
-                $fieldData = $block['data'][$field];
-                if (is_array($fieldData)) {
-                    foreach ($fieldData as $rowIndex => $value) {
-                        $fieldData[$rowIndex] = url('storage/' . ltrim($value, '/'));
-                    }
-                } else {
-                    $fieldData = url('storage/' . ltrim($fieldData, '/'));
-                }
-                $blocks[$index]['data'][$field] = $fieldData;
-            }
-        }
-
-        return $blocks;
-    }
 }
--- app/Services/PageService.php
+++ app/Services/PageService.php
@@ -33,9 +33,34 @@ class PageService
                 'is_active' => $page['is_active'],
             ],
             'metadata' => $page['metadata'] ?? [],
-            'blocks' => $blocks,
+            'blocks' => static::fixBlocksMediaLink($blocks),
         ];
 
         return $data;
+    }
+
+    private static function fixBlocksMediaLink($blocks)
+    {
+        // Full storage path
+        $blocksWithMedia = array_keys(Page::$mediaFieldsByBlockType);
+        foreach ($blocks as $index => $block) {
+            if (!in_array($block['type'], $blocksWithMedia)) {
+                continue;
+            }
+
+            foreach (Page::$mediaFieldsByBlockType[$block['type']] as $field) {
+                $fieldData = $block['data'][$field];
+                if (is_array($fieldData)) {
+                    foreach ($fieldData as $rowIndex => $value) {
+                        $fieldData[$rowIndex] = url('storage/' . ltrim($value, '/'));
+                    }
+                } else {
+                    $fieldData = url('storage/' . ltrim($fieldData, '/'));
+                }
+                $blocks[$index]['data'][$field] = $fieldData;
+            }
+        }
+
+        return $blocks;
     }
 }
